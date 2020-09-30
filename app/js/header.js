8a14d04cc90ba8726dd84470e3052b4d8c3b8288
Anton <Iarovoi>
2020-09-30 12:00:58 +0300

    form, new block

--- app/js/header.js
+++ app/js/header.js
@@ -2,7 +2,7 @@ const header = document.querySelector('.header');
 
 
 function hideHeader() {
-    if (window.pageYOffset >= 2526) {
+    if (window.pageYOffset >= 3250 && window.pageYOffset <= 4125) {
         header.style.position = 'static';
     } else {
         header.style.position = 'sticky';
